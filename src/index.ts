7162cb9735069ca615d53baad585e34d798404ce
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2021-12-09 14:22:11 +0100

    task complete

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { app } from './app.js'
+import { app } from './app'
 import mongoose from "mongoose"
+
+process.env.TS_NODE_DEV && require("dotenv").config()
 
 const port = process.env.PORT || 3001;
 
 console.log(process.env.MONGO_URL_CONNECTION)
 
-mongoose.connect(process.env.MONGO_URL_CONNECTION)
+mongoose.connect(process.env.MONGO_URL_CONNECTION!)
     .then(() => {
         console.log(`Connected to Mongo 🟢`);
         app.listen(port, () => {
